d3c12d3ecfdea19481c0820e80271072368df1d8
Srivatsan <vatsan1993@gmail.com>
2023-07-05 06:25:51 +0530

    all tests passing

--- class/ttt.js
+++ class/ttt.js
@@ -1,15 +1,15 @@
-const Screen = require("./screen");
-const Cursor = require("./cursor");
+const Screen = require('./screen');
+const Cursor = require('./cursor');
 
 class TTT {
-
   constructor() {
+    this.playerTurn = 'O';
 
-    this.playerTurn = "O";
-
-    this.grid = [[' ',' ',' '],
-                 [' ',' ',' '],
-                 [' ',' ',' ']]
+    this.grid = [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+    ];
 
     this.cursor = new Cursor(3, 3);
 
@@ -25,16 +25,73 @@ class TTT {
 
   // Remove this
   static testCommand() {
-    console.log("TEST COMMAND");
+    console.log('TEST COMMAND');
   }
 
   static checkWin(grid) {
-
     // Return 'X' if player X wins
     // Return 'O' if player O wins
-    // Return 'T' if the game is a tie
-    // Return false if the game has not ended
+    for (let row of grid) {
+      let valInRow = [];
+      for (let val of row) {
+        if (!valInRow.includes(val)) {
+          valInRow.push(val);
+        }
+      }
+      if (valInRow.length === 1 && valInRow[0] !== ' ') {
+        return valInRow[0];
+      }
+    }
+    // checking columns
+    for (let col = 0; col < grid[0].length; col++) {
+      let valInCol = [];
+      for (let row of grid) {
+        let val = row[col];
+        if (!valInCol.includes(val)) {
+          valInCol.push(val);
+        }
+      }
+      if (valInCol.length === 1 && valInCol[0] !== ' ') {
+        return valInCol[0];
+      }
+    }
+    // diag 1
+    let valInDiag = [];
+    for (let i = 0; i < grid.length; i++) {
+      let val = grid[i][i];
+      if (!valInDiag.includes(val)) {
+        valInDiag.push(val);
+      }
+    }
+    if (valInDiag.length === 1 && valInDiag[0] !== ' ') {
+      return valInDiag[0];
+    }
+    // diag 2
+    valInDiag = [];
+    for (let i = 0; i < grid.length; i++) {
+      let val = grid[i][grid.length - i - 1];
+      if (!valInDiag.includes(val)) {
+        valInDiag.push(val);
+      }
+    }
+    if (valInDiag.length === 1 && valInDiag[0] !== ' ') {
+      return valInDiag[0];
+    }
 
+    // Return 'T' if the game is a tie
+    let spaces = 0;
+    for (let row of grid) {
+      for (let val of row) {
+        if (val == ' ') {
+          spaces++;
+        }
+      }
+    }
+    if (spaces == 0) {
+      return 'T';
+    }
+    // Return false if the game has not ended
+    return false;
   }
 
   static endGame(winner) {
@@ -48,7 +105,6 @@ class TTT {
     Screen.render();
     Screen.quit();
   }
-
 }
 
 module.exports = TTT;
